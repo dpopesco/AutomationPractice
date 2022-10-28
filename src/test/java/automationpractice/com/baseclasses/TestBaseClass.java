1e6e115f41ca29e9bf10e29e38f4b67e7341d1b3
dpopesco <diana.popesco17@gmail.com>
2022-10-28 12:47:06 +0300

    Added comments, loadConfig method in TestBaseClass and config.properties file

--- src/test/java/automationpractice/com/baseclasses/TestBaseClass.java
+++ src/test/java/automationpractice/com/baseclasses/TestBaseClass.java
@@ -1,44 +1,92 @@
 package automationpractice.com.baseclasses;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class TestBaseClass {
 
+    //object for properties from configuration file
+    //marked as private to be visible only in this class
+    private Properties prop;
+
+    //marked as protected to be available in child classes
     protected WebDriver driver;
 
+
     @BeforeClass
-    public void startUpBrowser() {
+    public void startUpBrowser() throws Exception {
 
+        //called method to load from configuration file specified properties
+        //if configuration is not loaded throws an exception
+        if (!loadConfiguration()) {
+            throw new Exception("Configuration not loaded");
+        }
+        ;
+
+        //gets the project directory absolute path
         String localDir = System.getProperty("user.dir");
-        System.setProperty("web-driver.gecko.driver", localDir+"/geckodriver.exe");
+        //find user's path to firefox driver to start up FireFox Browser
+        System.setProperty("web-driver.gecko.driver", localDir + "/geckodriver.exe");
 
-        String localDir1 = System.getProperty("user.dir");
-        System.setProperty("web-driver.chrome.driver", localDir1+"/chromedriver.exe");
+        //find user's path to chrome driver to start up Chrome Browser
+        System.setProperty("web-driver.chrome.driver", localDir + "/chromedriver.exe");
 
-        driver = new FirefoxDriver();
-        driver.get("http://automationpractice.com/index.php");
+        //assign browser property from config.properties file to a new string variable
+        String browserName = prop.getProperty("browser");
 
-//        driver1 = new ChromeDriver();
-//        driver1.get("http://automationpractice.com/index.php");
+        //depending on the name given in config.properties for browser property,
+        //driver will use this browser for the tests
+        if (browserName.contains("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browserName.contains("firefox")) {
+            driver = new FirefoxDriver();
+        }
 
+        //receives the url given in config.properties
+        driver.get(prop.getProperty("url"));
+
+        //calls method which verifies that page is open by finding an element from homepage
         doBasicCheck();
     }
 
-    @Test(description = "No exception thrown by findElement considered a successful test")
-    public void checkSignInButtonIsPresent() {
-        driver.findElement(By.className("login"));
+    /**
+     * loads configuration and instantiate the prop attribute
+     *
+     * @return true if configuration was loaded
+     */
+    private boolean loadConfiguration() {
+        try {
+            //creates an object of class Properties
+            prop = new Properties();
+            //open config file using file input stream
+            FileInputStream ip = new FileInputStream(System
+                    .getProperty("user.dir") + "/configuration/config.properties");
+            //loading config.properties
+            prop.load(ip);
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
-
+    //method which verifies that page is open by finding an element from homepage
     private void doBasicCheck() {
         driver.findElement(By.id("header_logo"));
     }
 
+    //it will run regardless the results of tests (passed or failed)
     @AfterClass(alwaysRun = true)
     public void closeBrowser() {
         System.out.println("Closing down the browser");
--- src/test/java/automationpractice/com/ui/signin/Register.java
+++ src/test/java/automationpractice/com/ui/signin/Register.java
@@ -12,7 +12,10 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 public class Register extends TestBaseClass {
 
@@ -20,100 +23,140 @@ public class Register extends TestBaseClass {
 
     @Test
     public void withValidInputDataRegistersAndLogsInSuccessfully() {
-
+        //identifies Sign In button and clicks on it
         WebElement signInButton = driver.findElement(By.className("login"));
         signInButton.click();
 
+        //used implicit (30 s) and explicit wait for the web page to load and web element to appear
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id(emailId)));
 
-        String userNameGenerator = UUID.randomUUID().toString().substring(0, 10);
+        //generates random UniversallyUniqueID and extracts first 10 characters from it
+        String generatedUserName = UUID.randomUUID().toString().substring(0, 10);
+        //identifies email field
         WebElement emailRegister = driver.findElement(By.id(emailId));
-        String generatedEmail = userNameGenerator + "@sample.com";
+        //randomly generated UUID is concatenated with "@sample.com"
+        String generatedEmail = generatedUserName + "@sample.com";
+        //generated email is written in email field
         emailRegister.sendKeys(generatedEmail);
 
-        WebElement createAnAccountIcon = driver.findElement(By.id("SubmitCreate"));
-        createAnAccountIcon.click();
+        //identifies Create An Account Button and clicks on it
+        WebElement createAnAccountButton = driver.findElement(By.id("SubmitCreate"));
+        createAnAccountButton.click();
 
+        //used implicit (60 s) and explicit wait for the web page to load and heading element to appear
         wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h3[text()='Your personal information']")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//h3[text()='Your personal information']")));
 
+        //identifies page heading element and verifies that heading text is the same as expected
         WebElement createAccountHeading = driver.findElement(By.className("page-heading"));
         Assert.assertEquals(createAccountHeading.getText(), "CREATE AN ACCOUNT");
 
+        //identifies gender radiobutton and clicks on it
         WebElement gender = driver.findElement(By.id("id_gender2"));
         gender.click();
 
-        List<String> fName = Arrays.asList("Ecaterina", "Beatrice", "Ilinca", "Monica");
-        Collections.shuffle(fName);
-        int size1 = fName.size();
-        int item1 = new Random().nextInt(size1);
-        String generatedFName = fName.get(item1);
+        //creates a list of first names
+        List<String> firstNames = Arrays.asList("Ecaterina", "Beatrice", "Ilinca", "Monica");
+        //size() method returns number of elements in this list
+        int firstNamesSize = firstNames.size();
+        //generates a random number between 0 and the size of the array
+        int randomIndex = new Random().nextInt(0, firstNamesSize);
+        //returns first name at the specified position from array list
+        String randomFirstName = firstNames.get(randomIndex);
 
+        //identifies first name field and writes generated first name in it
         WebElement firstName = driver.findElement(By.id("customer_firstname"));
-        firstName.sendKeys(generatedFName);
+        firstName.sendKeys(randomFirstName);
 
-        List<String> lName = Arrays.asList("Potanga", "Popesco", "Miloshevici", "Bing");
-        int size2 = fName.size();
-        int item2 = new Random().nextInt(size2);
-        String generatedLName = lName.get(item2);
+        //creates a list of last names
+        List<String> lastNames = Arrays.asList("Potanga", "Popesco", "Miloshevici", "Bing");
+        //size() method returns number of elements in this list
+        int lastNamesSize = lastNames.size();
+        //generates a random number between 0 and the size of the array
+        randomIndex = new Random().nextInt(0, lastNamesSize);
+        //returns first name at the specified position from array list
+        String randomLastName = lastNames.get(randomIndex);
 
+        //identifies last name field and writes generated last name in it
         WebElement lastName = driver.findElement(By.id("customer_lastname"));
-        lastName.sendKeys(generatedLName);
+        lastName.sendKeys(randomLastName);
 
+        //identifies password field and writes in it
         WebElement password = driver.findElement(By.id("passwd"));
         password.sendKeys("Quatro");
 
+        //identifies address field and writes in it
         WebElement address = driver.findElement(By.id("address1"));
         address.sendKeys("SUA");
 
+        //identifies city field and writes in it
         WebElement city = driver.findElement(By.id("city"));
         city.sendKeys("Luisville");
 
+        //selects in dropdown button specified text
         Select drpStates = new Select(driver.findElement(By.name("id_state")));
         drpStates.selectByVisibleText("Kentucky");
 
+        //create an object of type Random with seed equal to current time in milliseconds
+        //to have always different values in random
         Random r = new Random(System.currentTimeMillis());
-        String randomPostcode = String.valueOf((1 + r.nextInt(2)) * 10000 + r.nextInt(10000));
+        //converts into string a random value from 20001 to 29999
+        String randomPostcode = String.valueOf(20000 + r.nextInt(10000));
+
+        //identifies postcode field and writes in it 5 digit random number
         WebElement postcode = driver.findElement(By.id("postcode"));
         postcode.sendKeys(randomPostcode);
 
-        Random r1 = new Random(System.currentTimeMillis());
-        String randomNumber = String.valueOf((1 + r.nextInt(2)) * 10000000 + r.nextInt(10000000));
-        WebElement mobilePhone = driver.findElement(By.id("phone_mobile"));
-        mobilePhone.sendKeys(randomNumber);
+        //converts into string a random value from 400000001 to 499999999
+        String randomMobile = String.valueOf(400000000 + r.nextInt(100000000));
 
+        //identifies mobile phone field and writes in it 9 digit random number
+        WebElement mobilePhone = driver.findElement(By.id("phone_mobile"));
+        mobilePhone.sendKeys(randomMobile);
+
+        //identifies Register button by xpath and clicks on it
         WebElement registerButton = driver.findElement(By.xpath("//button[@id='submitAccount']/span"));
         registerButton.click();
 
-
+        //assigned locator xpath in a String variable
         String accountNameXPath = "//a[@class='account']/span";
-        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        //used implicit (60 s) and explicit wait for the web page to load and web element to appear
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(accountNameXPath)));
 
+        //identifies account name web element after register
         WebElement accountName = driver.findElement(By.xpath(accountNameXPath));
-        Assert.assertEquals(accountName.getText(), generatedFName + " " + generatedLName);
+        //verify that account name is same as generated first and last name written previously
+        Assert.assertEquals(accountName.getText(), randomFirstName + " " + randomLastName);
 
+        //identifies Sign Out button and clicks on it
         WebElement signout = driver.findElement(By.className("logout"));
         signout.click();
 
+        //used implicit (30 s) and explicit wait for the web page to load and web element to appear
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h3[text()='Already registered?']")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//h3[text()='Already registered?']")));
 
-
+        //identifies email login field and writes in it previously registered email
         WebElement emailLogin = driver.findElement(By.id("email"));
         emailLogin.sendKeys(generatedEmail);
 
+        //identifies password login field and writes in it the password for previously registered account
         WebElement userPass = driver.findElement(By.id("passwd"));
         userPass.sendKeys("Quatro");
 
+        //identifies Log In button and clicks on it
         WebElement logInButton = driver.findElement(By.id("SubmitLogin"));
         logInButton.click();
 
+        //identifies Sign Out Button and assert that user logged in successfully
         WebElement signoutOption = driver.findElement(By.className("logout"));
         Assert.assertTrue(signoutOption.isDisplayed());
     }
 
+    // add test data set for negative testing using DataProvider annotation
     @DataProvider
     protected Object[][] invalidEmailProvider() {
         return new Object[][]{
@@ -123,25 +166,33 @@ public class Register extends TestBaseClass {
         };
     }
 
+    // the invalidEmail parameter is valorized by invalidEmailProvider
+    // meaning that this test will be called 3 times with values from DataProvider
     @Test(dataProvider = "invalidEmailProvider")
     public void withInvalidEmailAddressInputShowsError(String invalidEmail) {
 
+        //identifies Sign In button and clicks on it
         WebElement signInButton = driver.findElement(By.className("login"));
         signInButton.click();
 
+        //used implicit (30 s) and explicit wait for the web page to load and web element to appear
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id(emailId)));
 
+        //identifies email field and writes from data provider invalid email
         WebElement emailField = driver.findElement(By.id(emailId));
         emailField.sendKeys(invalidEmail);
 
-        WebElement emailElement = driver.findElement(By.xpath("//div/label[@for='email_create']"));
+        //identifies email label above the email field by xpath
+        WebElement emailLabel = driver.findElement(By.xpath("//div/label[@for='email_create']"));
+        //move to label element by clicking
         Actions action = new Actions(driver);
-        action.moveToElement(emailField).moveToElement(emailElement).click().build().perform();
+        action.moveToElement(emailLabel).click().build().perform();
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        //used implicit (30 s) and explicit wait for the web page to load and css class to appear
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".form-error")));
 
+        //verifies that class .form-error appears when we write wrong format emails
         WebElement errorElement = driver.findElement(By.cssSelector(".form-error"));
         Assert.assertTrue(errorElement.isDisplayed());
     }
